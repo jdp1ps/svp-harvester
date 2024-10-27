5ebbcb05516f5d3e668834e06a0bffcff0793323
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-27 10:26:35 +0100

    Add index to concepts join table

--- app/db/models/references_subject.py
+++ app/db/models/references_subject.py
@@ -5,6 +5,6 @@ from app.db.session import Base
 references_subjects_table = Table(
     "references_subjects_table",
     Base.metadata,
-    Column("reference_id", ForeignKey("references.id")),
-    Column("concept_id", ForeignKey("concepts.id")),
+    Column("reference_id", ForeignKey("references.id"), index=True),
+    Column("concept_id", ForeignKey("concepts.id"), index=True),
 )
