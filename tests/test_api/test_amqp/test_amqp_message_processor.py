b4a1d446138279226688f11e16fdaaf3d518230e
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-27 08:47:51 +0100

    Add missing fixture parameter

--- tests/test_api/test_amqp/test_amqp_message_processor.py
+++ tests/test_api/test_amqp/test_amqp_message_processor.py
@@ -35,9 +35,9 @@ def fixture_mock_retrieval_service_run():
 
 
 @pytest.fixture(name="message_processor")
-async def fixture_message_processor() -> AMQPMessageProcessor:
+async def fixture_message_processor(test_app) -> AMQPMessageProcessor:
     """AMQP message processor fixture to use AMQPInterface as factory."""
-    amqp_connexion = AMQPInterface(get_app_settings())
+    amqp_connexion = AMQPInterface(get_app_settings(), state=test_app.state)
     # pylint: disable=protected-access
     return await amqp_connexion._message_processor()
 
