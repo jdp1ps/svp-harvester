f0d1519ab9c0fb490e542816042e05152b0eee5a
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-27 10:10:03 +0100

    Add index to abstracts

--- app/db/models/abstract.py
+++ app/db/models/abstract.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Text
+from sqlalchemy import Text, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from app.db.models.reference_literal_field import ReferenceLiteralField
@@ -11,4 +11,7 @@ class Abstract(ReferenceLiteralField):
     __mapper_args__ = {"concrete": True}
 
     value: Mapped[str] = mapped_column(Text, nullable=False, index=False)
+    reference_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("references.id"), nullable=False, index=True
+    )
     reference = relationship("Reference", back_populates="abstracts", lazy="raise")
