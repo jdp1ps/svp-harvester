fb621e23611f80e7942b23bbf8f7d8c0c1d56c79
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-27 10:29:34 +0100

    Add index to document types

--- app/db/models/references_document_type.py
+++ app/db/models/references_document_type.py
@@ -5,6 +5,6 @@ from app.db.session import Base
 references_document_type_table = Table(
     "references_document_types_table",
     Base.metadata,
-    Column("reference_id", ForeignKey("references.id")),
-    Column("document_type_id", ForeignKey("document_types.id")),
+    Column("reference_id", ForeignKey("references.id"), index=True),
+    Column("document_type_id", ForeignKey("document_types.id"), index=True),
 )
