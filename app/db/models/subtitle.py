54e1c0498364519bfb10e33f3a36caf01024133e
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-27 10:23:25 +0100

    Add index to subtitles

--- app/db/models/subtitle.py
+++ app/db/models/subtitle.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from app.db.models.reference_literal_field import ReferenceLiteralField
 
@@ -8,6 +9,8 @@ class Subtitle(ReferenceLiteralField):
 
     __tablename__ = "subtitles"
     __mapper_args__ = {"concrete": True}
+
+    reference_id: Mapped[int] = mapped_column(ForeignKey("references.id"), index=True)
 
     reference: Mapped["app.db.models.reference.Reference"] = relationship(
         "app.db.models.reference.Reference", back_populates="subtitles", lazy="raise"
