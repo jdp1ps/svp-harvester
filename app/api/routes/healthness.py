902bab63cae0f8017d993b9d1474266e8cad0fb9
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-28 07:40:06 +0100

    Make health route async

--- app/api/routes/healthness.py
+++ app/api/routes/healthness.py
@@ -23,7 +23,7 @@ router = APIRouter()
     status_code=status.HTTP_200_OK,
     response_model=HealthCheck,
 )
-def get_health(request: Request, response: Response) -> HealthCheck:
+async def get_health(request: Request, response: Response) -> HealthCheck:
     """
     ## Perform a Health Check
     Endpoint to perform a healthcheck on.
